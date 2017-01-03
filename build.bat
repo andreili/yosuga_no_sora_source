20c4946d27edd82c6e56eebcaf33319e1187a023
andreil <andreil499@gmail.com>
2017-01-04 01:17:41 +0300

    Дополнительное разбиение по файлам

--- build.bat
+++ build.bat
@@ -11,5 +11,10 @@ bin\cotoco.exe /nologo /Fo out\AppreciationView.co AppreciationView.cos | bin\ic
 bin\cotoco.exe /nologo /Fo out\yosuga.co yosuga.cos | bin\iconv -f SHIFT_JIS -t UTF-8
 bin\cotoco.exe /nologo /Fo out\bgm.co bgm.cos | bin\iconv -f SHIFT_JIS -t UTF-8
 bin\cotoco.exe /nologo /Fo out\BustupManager.co BustupManager.cos | bin\iconv -f SHIFT_JIS -t UTF-8
-bin\cotolink.exe /nologo /Lo out\yosuga_rev.csx out\cotopha.co out\init.co out\structs.co out\ConfigDataBase.co out\SystemObject.co out\start_screens.co out\yosuga.co out\AdvScreen.co out\AppreciationView.co out\bgm.co out\BustupManager.co | bin\iconv -f SHIFT_JIS -t UTF-8
+bin\cotoco.exe /nologo /Fo out\EyeCatch.co EyeCatch.cos | bin\iconv -f SHIFT_JIS -t UTF-8
+bin\cotoco.exe /nologo /Fo out\Debug.co Debug.cos | bin\iconv -f SHIFT_JIS -t UTF-8
+bin\cotoco.exe /nologo /Fo out\Input.co Input.cos | bin\iconv -f SHIFT_JIS -t UTF-8
+bin\cotoco.exe /nologo /Fo out\Flag.co Flag.cos | bin\iconv -f SHIFT_JIS -t UTF-8
+bin\cotoco.exe /nologo /Fo out\ConfigWindow.co ConfigWindow.cos | bin\iconv -f SHIFT_JIS -t UTF-8
+bin\cotolink.exe /nologo /Lo out\yosuga_rev.csx out\cotopha.co out\init.co out\structs.co out\ConfigDataBase.co out\SystemObject.co out\start_screens.co out\yosuga.co out\AdvScreen.co out\AppreciationView.co out\bgm.co out\BustupManager.co out\EyeCatch.co out\Debug.co out\Input.co out\Flag.co out\ConfigWindow.co | bin\iconv -f SHIFT_JIS -t UTF-8
 bin\sora_tool.exe --export --input out/yosuga_rev.csx --output ../data/
