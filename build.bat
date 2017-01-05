b8a9b74e2abbf2132c5e1c3a02410682c2b8873a
andreil <andreil499@gmail.com>
2017-01-05 18:57:34 +0300

    Добавлен класс LoadSaveWindow

--- build.bat
+++ build.bat
@@ -1,20 +1,25 @@
 @echo off
-@chcp 65001
-bin\cotoco.exe /nologo /Fo out\cotopha.co cotopha.cos | bin\iconv -f SHIFT_JIS -t UTF-8
-bin\cotoco.exe /nologo /Fo out\init.co init.cos | bin\iconv -f SHIFT_JIS -t UTF-8
-bin\cotoco.exe /nologo /Fo out\structs.co structs.cos | bin\iconv -f SHIFT_JIS -t UTF-8
-bin\cotoco.exe /nologo /Fo out\ConfigDataBase.co ConfigDataBase.cos | bin\iconv -f SHIFT_JIS -t UTF-8
-bin\cotoco.exe /nologo /Fo out\SystemObject.co SystemObject.cos | bin\iconv -f SHIFT_JIS -t UTF-8
-bin\cotoco.exe /nologo /Fo out\start_screens.co start_screens.cos | bin\iconv -f SHIFT_JIS -t UTF-8
-bin\cotoco.exe /nologo /Fo out\AdvScreen.co AdvScreen.cos | bin\iconv -f SHIFT_JIS -t UTF-8
-bin\cotoco.exe /nologo /Fo out\AppreciationView.co AppreciationView.cos | bin\iconv -f SHIFT_JIS -t UTF-8
-bin\cotoco.exe /nologo /Fo out\yosuga.co yosuga.cos | bin\iconv -f SHIFT_JIS -t UTF-8
-bin\cotoco.exe /nologo /Fo out\bgm.co bgm.cos | bin\iconv -f SHIFT_JIS -t UTF-8
-bin\cotoco.exe /nologo /Fo out\BustupManager.co BustupManager.cos | bin\iconv -f SHIFT_JIS -t UTF-8
-bin\cotoco.exe /nologo /Fo out\EyeCatch.co EyeCatch.cos | bin\iconv -f SHIFT_JIS -t UTF-8
-bin\cotoco.exe /nologo /Fo out\Debug.co Debug.cos | bin\iconv -f SHIFT_JIS -t UTF-8
-bin\cotoco.exe /nologo /Fo out\Input.co Input.cos | bin\iconv -f SHIFT_JIS -t UTF-8
-bin\cotoco.exe /nologo /Fo out\Flag.co Flag.cos | bin\iconv -f SHIFT_JIS -t UTF-8
-bin\cotoco.exe /nologo /Fo out\ConfigWindow.co ConfigWindow.cos | bin\iconv -f SHIFT_JIS -t UTF-8
-bin\cotolink.exe /nologo /Lo out\yosuga_rev.csx out\cotopha.co out\init.co out\structs.co out\ConfigDataBase.co out\SystemObject.co out\start_screens.co out\yosuga.co out\AdvScreen.co out\AppreciationView.co out\bgm.co out\BustupManager.co out\EyeCatch.co out\Debug.co out\Input.co out\Flag.co out\ConfigWindow.co | bin\iconv -f SHIFT_JIS -t UTF-8
-bin\sora_tool.exe --export --input out/yosuga_rev.csx --output ../data/
+rem @chcp 65001
+bin\cotoco.exe /nologo /Fo out\cotopha.co cotopha.cos
+bin\cotoco.exe /nologo /Fo out\init.co init.cos
+bin\cotoco.exe /nologo /Fo out\structs.co structs.cos
+bin\cotoco.exe /nologo /Fo out\ConfigDataBase.co ConfigDataBase.cos
+bin\cotoco.exe /nologo /Fo out\SystemObject.co SystemObject.cos
+bin\cotoco.exe /nologo /Fo out\start_screens.co start_screens.cos
+bin\cotoco.exe /nologo /Fo out\AdvScreen.co AdvScreen.cos
+bin\cotoco.exe /nologo /Fo out\AppreciationView.co AppreciationView.cos
+bin\cotoco.exe /nologo /Fo out\yosuga.co yosuga.cos
+bin\cotoco.exe /nologo /Fo out\bgm.co bgm.cos
+bin\cotoco.exe /nologo /Fo out\BustupManager.co BustupManager.cos
+bin\cotoco.exe /nologo /Fo out\EyeCatch.co EyeCatch.cos
+bin\cotoco.exe /nologo /Fo out\Debug.co Debug.cos
+bin\cotoco.exe /nologo /Fo out\Input.co Input.cos
+bin\cotoco.exe /nologo /Fo out\Flag.co Flag.cos
+bin\cotoco.exe /nologo /Fo out\ConfigWindow.co ConfigWindow.cos
+bin\cotoco.exe /nologo /Fo out\vars.co vars.cos
+bin\cotoco.exe /nologo /Fo out\HistoryWindow.co HistoryWindow.cos
+bin\cotoco.exe /nologo /Fo out\LogManager.co LogManager.cos
+bin\cotoco.exe /nologo /Fo out\MessageFrame.co MessageFrame.cos
+bin\cotoco.exe /nologo /Fo out\LoadSaveWindow.co LoadSaveWindow.cos
+bin\cotolink.exe /nologo /Lo out\yosuga.csx out\cotopha.co out\init.co out\structs.co out\ConfigDataBase.co out\SystemObject.co out\start_screens.co out\yosuga.co out\AdvScreen.co out\AppreciationView.co out\bgm.co out\BustupManager.co out\EyeCatch.co out\Debug.co out\Input.co out\Flag.co out\ConfigWindow.co out\vars.co out\HistoryWindow.co out\LogManager.co out\MessageFrame.co out\LoadSaveWindow.co
+bin\sora_tool.exe --export --input out/yosuga.csx --output ../data/
