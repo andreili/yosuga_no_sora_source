9d59898937c099663f229d01212e065e96c7e999
andreil <andreil499@gmail.com>
2017-01-07 03:14:04 +0300

    Добавлена первая часть сценария

--- build.bat
+++ build.bat
@@ -24,6 +24,41 @@ bin\cotoco.exe /nologo /Fo out\LoadSaveWindow.co LoadSaveWindow.cos
 bin\cotoco.exe /nologo /Fo out\utils.co utils.cos
 bin\cotoco.exe /nologo /Fo out\NovelLogManager.co NovelLogManager.cos
 bin\cotoco.exe /nologo /Fo out\Scenario.co Scenario.cos
-bin\cotoco.exe /nologo /Fo out\sc00_Z000.co sc00_Z000.cos
-bin\cotolink.exe /nologo /Lo out\yosuga.csx out\cotopha.co out\init.co out\structs.co out\ConfigDataBase.co out\SystemObject.co out\start_screens.co out\yosuga.co out\AdvScreen.co out\AppreciationView.co out\bgm.co out\BustupManager.co out\EyeCatch.co out\Debug.co out\Input.co out\Flag.co out\ConfigWindow.co out\vars.co out\HistoryWindow.co out\LogManager.co out\MessageFrame.co out\LoadSaveWindow.co out\utils.co out\NovelLogManager.co out\Scenario.co out\Scenario.co out\sc00_Z000.co
-rem bin\sora_tool.exe --export --input out/yosuga.csx --output ../data/
+bin\cotoco.exe /nologo /Fo out\sc00_A001.co scenes\sc00_A001.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A002.co scenes\sc00_A002.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A003.co scenes\sc00_A003.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A004.co scenes\sc00_A004.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A005.co scenes\sc00_A005.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A006.co scenes\sc00_A006.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A007.co scenes\sc00_A007.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A008.co scenes\sc00_A008.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A009.co scenes\sc00_A009.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A010.co scenes\sc00_A010.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A011.co scenes\sc00_A011.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A012.co scenes\sc00_A012.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A013.co scenes\sc00_A013.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A014.co scenes\sc00_A014.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A015A.co scenes\sc00_A015A.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A015B.co scenes\sc00_A015B.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A015C.co scenes\sc00_A015C.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A016A.co scenes\sc00_A016A.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A016B.co scenes\sc00_A016B.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A016C.co scenes\sc00_A016C.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A017.co scenes\sc00_A017.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A018A.co scenes\sc00_A018A.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A018B.co scenes\sc00_A018B.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A018C.co scenes\sc00_A018C.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A019.co scenes\sc00_A019.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A020.co scenes\sc00_A020.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A021.co scenes\sc00_A021.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A022.co scenes\sc00_A022.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A023A.co scenes\sc00_A023A.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A023B.co scenes\sc00_A023B.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A024.co scenes\sc00_A024.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A025A.co scenes\sc00_A025A.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A025B.co scenes\sc00_A025B.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A025C.co scenes\sc00_A025C.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A026.co scenes\sc00_A026.cos
+bin\cotoco.exe /nologo /Fo out\sc00_A027.co scenes\sc00_A027.cos
+bin\cotolink.exe /nologo /Lo out\yosuga.csx out\cotopha.co out\init.co out\structs.co out\ConfigDataBase.co out\SystemObject.co out\start_screens.co out\yosuga.co out\AdvScreen.co out\AppreciationView.co out\bgm.co out\BustupManager.co out\EyeCatch.co out\Debug.co out\Input.co out\Flag.co out\ConfigWindow.co out\vars.co out\HistoryWindow.co out\LogManager.co out\MessageFrame.co out\LoadSaveWindow.co out\utils.co out\NovelLogManager.co out\Scenario.co out\Scenario.co out\sc00_A001.co out\sc00_A002.co out\sc00_A003.co out\sc00_A004.co out\sc00_A005.co out\sc00_A006.co out\sc00_A007.co out\sc00_A008.co out\sc00_A009.co out\sc00_A010.co out\sc00_A011.co out\sc00_A012.co out\sc00_A013.co out\sc00_A014.co out\sc00_A015A.co out\sc00_A015B.co out\sc00_A015C.co out\sc00_A016A.co out\sc00_A016B.co out\sc00_A016C.co out\sc00_A017.co out\sc00_A018A.co out\sc00_A018B.co out\sc00_A018C.co out\sc00_A019.co out\sc00_A020.co out\sc00_A021.co out\sc00_A022.co out\sc00_A023A.co out\sc00_A023B.co out\sc00_A024.co out\sc00_A025A.co out\sc00_A025B.co out\sc00_A025C.co out\sc00_A026.co out\sc00_A027.co
+bin\sora_tool.exe --export --input out/yosuga.csx --output ../data/
